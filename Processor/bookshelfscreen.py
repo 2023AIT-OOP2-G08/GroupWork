24a9e1a77eb922dca86f3c82eaac155712feea6b
Odagiri01 <k22031kk@aitech.ac.jp>
2024-01-17 11:14:24 +0900

    コードを修正、追加しました

--- Processor/bookshelfscreen.py
+++ Processor/bookshelfscreen.py
@@ -1,14 +1,21 @@
 import json
 
+__all__ = ['bookshelfscreen']
+
 def bookshelfscreen(json_file_path):
     with open(json_file_path, 'r', encoding='utf-8') as file:  #jsonファイルを開ける
 
         data = json.load(file) #dataにjsonファイルの情報を代入
         info_list = [ #listに画像、タイトル、JSBNの順に情報を代入
-            data.get("cover_image_url", ""),
-            data.get("title", ""),
-            data.get("isbn_13", "")
-        ]
+            data['title'],
+            data['isbn_13'],
+            data['authors'],
+            data['published_date'],
+            data['page_count'],
+            data['description'],
+            data['publisher'],
+            data['cover_image_url']
+            ]
 
     return info_list #リストを返す
 
