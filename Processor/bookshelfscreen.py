d3d746a56205c120fba223fcf1b6331df98e6022
Odagiri01 <k22031kk@aitech.ac.jp>
2024-01-18 16:16:13 +0900

    syuudsei

--- Processor/bookshelfscreen.py
+++ Processor/bookshelfscreen.py
@@ -15,7 +15,6 @@ def bookshelfscreen(json_file_path):
             data['publisher'],
             data['cover_image_url']
             ]
-
     return info_list #リストを返す
 
 def remove_index_elements(input_list, index):
@@ -31,7 +30,6 @@ def add_information(input_list, new_list):
     #データを追加するプログラム
     #input_list お気に入りした本を格納しているlist 既存のlist
     #新しく追加するデータnew_list(list)
-    
     input_list.append(new_list)
     return input_list
 
@@ -58,6 +56,7 @@ def append_to_json(json_file_path, info_list):
     with open(json_file_path, 'w', encoding='utf-8') as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
 
+#-------------------------------------------------------------------------------#
 # 例として、json_file_pathとinfo_listを適当に設定
 json_file_path = 'Bookshelf.json'
 info_list = ['test', '1234567', ['Author'], '2024-01-18', 200, 'New book description', 'Publisher', 'https://example.com/cover.jpg']
