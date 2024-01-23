3c43c9033b7d0ff1dd7febca78792995801c9549
mizuryo108 <153165016+mizuryo108@users.noreply.github.com>
2024-01-24 01:15:30 +0900

    Update BookDetailsScreen.py
    ・書籍情報のラベルテキストを修正しました
    ・クラスに変換後の画像が表示されない不具合の解決しました
    ・柘植さんの実装方法を真似しようとしたがよく理解できなかったため、Textウィジェットを使用しました
    ・要約部分のみに対応するスクロールバーが画面全体の右端に表示されるため不自然に感じますが、一応コメント化してあります
    ・Textウィジェット部分(要約表示部分)をスライドするとスクロールできるため、スクロールバーが無くても大丈夫にはなっています

--- Screen/BookDetailsScreen.py
+++ Screen/BookDetailsScreen.py
@@ -1,126 +1,166 @@
 import tkinter as tk
+from tkinter import Scrollbar, Text
 from PIL import Image, ImageTk
 import urllib.request
 import io
 import textwrap
+from BaseScreen import BaseScreen
 
-data_list = [
-    "情熱プログラマー ソフトウェア開発者の幸せな生き方",
-    "9784274067938",
-    "No publisher information available",
-    "2010-02",
-    200,
-    '本書は、等身大のプログラマの一人がキャリア開発の重要性を説き、そのための心構えなどを示したもの。「プログラマはビジネス視点を持って意識的なキャリア開発をすべき」という視点から、その実践方法を著者独特の生き生きとした共感できる語り口で伝える。原書は"The Passionate Programmer: Creating a Remarkable Career in Software Development"(The Pragmatic Programmers,2009)',
-    "株式会社 オーム社",
-    "http://books.google.com/books/content?id=oFrK2r8rQbEC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
-]
+"""
+TODO: 画面遷移するクラスをインポート
+# from BookDetailsScreen import BookSearchScreen # 詳細画面
+# from BookShelfScreen import BookShelfScreen # 本棚画面
 
-# 著者名が表記されていて配列でリストに格納されている場合
-# 要素の間に改行を加えて一つの文に変換する
-if data_list[2] != "No publisher information available":
-    data_list[2] = "\n".join(data_list[2])
+"""
 
-# Tkクラス生成
+SCREEN_SIZE = "600x600"  # 画面サイズ
+FONT_TYPE = "Helvetica"  # フォント
+
+
+class BookDetailsScreen(BaseScreen):
+    def create_widgets(self):
+        data_list = [
+            "UIデザインの教科書［新版］ マルチデバイス時代のインターフェース設計",
+            "9784798155456",
+            ["原田秀司"],
+            "2019-01-21",
+            210,
+            "使いやすい理由とは何か 本書はUIにおけるデザインの定義から、 ハードおよびソフトによる制約、人間の心理による影響、 そして具体的にデザインを形にする方法までを、 図や画像を使いながら、わかりやすく体系的に解説していきます。 Webサイトの閲覧者やアプリのユーザーは、 いつのまにか迷ったり、わからなくなったり、 操作がしっくりこなかったりすることがあります。 本書を読むと「わかりやすさ」と「使いやすさ」の要点がわかるので、 ユーザーを迷わせない、最適なUIを見つけることができます。 デザイナーはもちろん、エンジニア、ディレクター、発注者など、 UI制作に関わる、あらゆる方におすすめの1冊です。 ＊本書は2013年刊行の『UIデザインの教科書』をもとにしていますが、 最新環境にあわせて、構成及び内容を全面的に書き直しています。 〈こんな人のための本です〉 ・UIデザインの基本的な考え方を学びたい ・わかりやすさや使いやすさの理由が知りたい ・最新のデバイスごとの違いやルールを知りたい ・UIデザインのチェック項目が知りたい ・UIデザインを説明するためのロジックが学びたい ...etc 〈目次〉 第1章 デザインの目的とUI/UX 第2章 物理的な制約 第3章 ソフトウェアの影響 第4章 人間の認知特性 第5章 階層と構造 第6章 ナビゲーションとインタラクション 第7章 デザインを形にする",
+            "翔泳社",
+            "http://books.google.com/books/content?id=oH2GDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        ]
+
+        # 著者名が表記されていて配列でリストに格納されている場合
+        # 要素の間に改行を加えて一つの文に変換する
+        if data_list[2] != "No publisher information available":
+            data_list[2] = "\n".join(data_list[2])
+
+        # 画面タイトル
+        root.title("書籍詳細画面")
+        # 画面サイズ
+        root.geometry(SCREEN_SIZE)
+
+        # タイトルの作成
+        label2 = tk.Label(self.frame, text="書籍詳細画面", font=(FONT_TYPE, 30, "bold"))
+        label2.place(x=207, y=20)
+
+        # 表紙の画像表示
+        url = data_list[7]
+
+        # URLから画像データをダウンロード
+        with urllib.request.urlopen(url) as u:
+            raw_data = u.read()
+
+        # ダウンロードした画像データをPIL.Imageオブジェクトに変換
+        im = Image.open(io.BytesIO(raw_data))
+
+        # PIL.Imageオブジェクトをtkinterで使用可能なImageTk.PhotoImageオブジェクトに変換
+        self.photo = ImageTk.PhotoImage(im)
+
+        # ラベルに画像を設定してウィンドウに追加
+        label_photo = tk.Label(self.frame, image=self.photo)
+        label_photo.place(x=20, y=140)
+
+        # 項目ごとのラベルの作成
+        # タイトルの項目名、jsonから得た情報の表示ラベル
+        label3 = tk.Label(self.frame, text="タイトル : ", font=(FONT_TYPE, 17, "bold"))
+        label3.place(x=180, y=140)
+
+        label_title = tk.Label(
+            self.frame,
+            text=("\n".join(data_list[0].split())),
+            font=(FONT_TYPE, 12, "bold"),
+        )
+        label_title.place(x=180, y=180)
+
+        # 著者の項目名、jsonから得た情報の表示ラベル
+        label4 = tk.Label(self.frame, text="著者 : ", font=(FONT_TYPE, 17, "bold"))
+        label4.place(x=400, y=140)
+
+        label_authors = tk.Label(
+            self.frame, text=(data_list[2]), font=(FONT_TYPE, 12, "bold")
+        )
+        label_authors.place(x=400, y=180)
+
+        # ISBN-13の項目名、jsonから得た情報の表示ラベル
+        label5 = tk.Label(self.frame, text="ISBN-13 : ", font=(FONT_TYPE, 17, "bold"))
+        label5.place(x=180, y=240)
+
+        label_isbn_13 = tk.Label(
+            self.frame, text=data_list[1], font=(FONT_TYPE, 12, "bold")
+        )
+        label_isbn_13.place(x=180, y=280)
+
+        # 出版社の項目名、jsonから得た情報の表示ラベル
+        label6 = tk.Label(self.frame, text="出版社 : ", font=(FONT_TYPE, 17, "bold"))
+        label6.place(x=400, y=240)
+
+        label_publisher = tk.Label(
+            self.frame, text=data_list[6], font=(FONT_TYPE, 12, "bold")
+        )
+        label_publisher.place(x=400, y=280)
+
+        # ページ数の項目名、jsonから得た情報の表示ラベル
+        label7 = tk.Label(self.frame, text="ページ数 : ", font=(FONT_TYPE, 17, "bold"))
+        label7.place(x=180, y=340)
+
+        label_page_count = tk.Label(
+            self.frame, text=data_list[4], font=(FONT_TYPE, 12, "bold")
+        )
+        label_page_count.place(x=180, y=380)
+
+        # 出版日の項目名、jsonから得た情報の表示ラベル
+        label8 = tk.Label(self.frame, text="出版日 : ", font=(FONT_TYPE, 17, "bold"))
+        label8.place(x=400, y=340)
+
+        label_published_date = tk.Label(
+            self.frame, text=data_list[3], font=(FONT_TYPE, 12, "bold")
+        )
+        label_published_date.place(x=400, y=380)
+
+        # 要約の項目名、jsonから得た情報の表示ラベル
+        label9 = tk.Label(self.frame, text="要約 : ", font=(FONT_TYPE, 17, "bold"))
+        label9.place(x=50, y=440)
+
+        def on_scroll(*args):
+            text.yview(*args)
+
+        # Textウィジェットを作成
+        text = tk.Text(root, wrap="none", width=69, height=8)
+        text.pack(side="left", fill="both", expand=True)
+        text.place(x=50, y=480)
+
+        """# スクロールバーを作成
+        scrollbar = tk.Scrollbar(root, command=on_scroll)
+        scrollbar.pack(side="right", fill="y")
+
+        # スクロールバーとTextウィジェットを関連付ける
+        text.config(yscrollcommand=scrollbar.set)"""
+
+        # 40文字ごとで改行
+        description_data = textwrap.fill(data_list[5], 40)
+        # print(type(data_list[5]))
+        # テキストを追加
+        for ch in description_data:
+            text.insert("end", ch)
+
+        # 画面遷移(書籍詳細画面のウィンドウを閉じる)
+        def close_window():
+            root.destroy()
+
+        button = tk.Button(text="前の画面へ", command=close_window)
+        button.pack()
+        button.place(x=20, y=80)
+
+
+# tkinterウィンドウを作成
 root = tk.Tk()
 
-# 画面タイトル
-root.title("書籍詳細画面")
+# クラスをインスタンス化
+book_details_screen = BookDetailsScreen(root)
 
-# 画面サイズ
-root.geometry("600x600")
+# 画面を表示
+book_details_screen.screen_show()
 
-# タイトルの作成
-label1 = tk.Label(root, text="書籍管理アプリ", font=("Helvetica", 20, "bold"))
-label1.place(x=20, y=25)
-
-label2 = tk.Label(root, text="書籍詳細画面", font=("Helvetica", 30, "bold"))
-label2.place(x=207, y=20)
-
-
-# 表紙の画像表示
-url = data_list[7]
-
-# URLから画像データをダウンロード
-with urllib.request.urlopen(url) as u:
-    raw_data = u.read()
-
-# ダウンロードした画像データをPIL.Imageオブジェクトに変換
-im = Image.open(io.BytesIO(raw_data))
-
-# PIL.Imageオブジェクトをtkinterで使用可能なImageTk.PhotoImageオブジェクトに変換
-photo = ImageTk.PhotoImage(im)
-
-# ラベルに画像を設定してウィンドウに追加
-label_photo = tk.Label(root, image=photo)
-label_photo.pack()
-label_photo.place(x=20, y=140)
-
-
-# 項目ごとのラベルの作成
-# タイトルの項目名、jsonから得た情報の表示ラベル
-label3 = tk.Label(root, text="タイトル : title", font=("Helvetica", 17, "bold"))
-label3.place(x=180, y=140)
-
-label_title = tk.Label(
-    root, text=("\n".join(data_list[0].split())), font=("Helvetica", 12, "bold")
-)
-label_title.place(x=180, y=180)
-
-# 著者の項目名、jsonから得た情報の表示ラベル
-label4 = tk.Label(root, text="著者 : authors", font=("Helvetica", 17, "bold"))
-label4.place(x=400, y=140)
-
-label_authors = tk.Label(root, text=(data_list[2]), font=("Helvetica", 12, "bold"))
-label_authors.place(x=400, y=180)
-
-# ISBN-13の項目名、jsonから得た情報の表示ラベル
-label5 = tk.Label(root, text="ISBN-13 : isbn_13", font=("Helvetica", 17, "bold"))
-label5.place(x=180, y=240)
-
-label_isbn_13 = tk.Label(root, text=data_list[1], font=("Helvetica", 12, "bold"))
-label_isbn_13.place(x=180, y=280)
-
-# 出版社の項目名、jsonから得た情報の表示ラベル
-label6 = tk.Label(root, text="出版社 : publisher", font=("Helvetica", 17, "bold"))
-label6.place(x=400, y=240)
-
-label_publisher = tk.Label(root, text=data_list[6], font=("Helvetica", 12, "bold"))
-label_publisher.place(x=400, y=280)
-
-# ページ数の項目名、jsonから得た情報の表示ラベル
-label7 = tk.Label(root, text="ページ数 : page_count", font=("Helvetica", 17, "bold"))
-label7.place(x=180, y=340)
-
-label_page_count = tk.Label(root, text=data_list[4], font=("Helvetica", 12, "bold"))
-label_page_count.place(x=180, y=380)
-
-# 出版日の項目名、jsonから得た情報の表示ラベル
-label8 = tk.Label(root, text="出版日 : published_date", font=("Helvetica", 17, "bold"))
-label8.place(x=400, y=340)
-
-label_published_date = tk.Label(root, text=data_list[3], font=("Helvetica", 12, "bold"))
-label_published_date.place(x=400, y=380)
-
-# 要約の項目名、jsonから得た情報の表示ラベル
-label9 = tk.Label(root, text="要約 : description", font=("Helvetica", 17, "bold"))
-label9.place(x=50, y=440)
-
-label_description = tk.Label(
-    root, text=(textwrap.fill(data_list[5], 40)), font=("Helvetica", 12, "bold")
-)
-label_description.place(x=50, y=480)
-
-
-# 画面遷移(書籍詳細画面のウィンドウを閉じる)
-def close_window():
-    root.destroy()
-
-
-# 画面遷移用のボタン
-button = tk.Button(text="前の画面へ", command=close_window)
-button.pack()
-button.place(x=20, y=80)
-
-
-# メインループ実行
+### ウィンドウを表示
 root.mainloop()
