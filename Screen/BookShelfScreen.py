a8a0fb7aefcd301f904b78aef927ce1491848761
shinno22085 <148172102+shinno22085@users.noreply.github.com>
2024-01-25 12:32:03 +0900

    Update BookShelfScreen.py

--- Screen/BookShelfScreen.py
+++ Screen/BookShelfScreen.py
@@ -1,11 +1,40 @@
 import tkinter as tk
 from tkinter import ttk
+from BaseScreen import BaseScreen  # BaseScreenクラスをインポート
 
-#削除ボタンを押したときにその行を一斉削除
-def delete_item():
-    selected_item = tbl.selection()
-    if selected_item:
-        tbl.delete(selected_item)
+class BookShelfScreen(BaseScreen):
+    def __init__(self, root: tk.Tk):
+        # BaseScreenクラスのコンストラクタを呼び出す
+        super().__init__(root)
+
+    def create_widgets(self):
+        # create_widgetsメソッドをオーバーライドしてBookShelfScreenのウィジェットを定義する
+        label2 = tk.Label(self.frame, text="本棚画面", font=("Helvetica", 20, "bold"))
+        label2.place(x=250, y=20)
+
+        tbl = ttk.Treeview(self.frame)
+        tbl.place(x=50, y=80)
+
+        tbl['columns'] = ('cover', 'title', 'number', 'button')
+        tbl['show'] = 'headings'
+        tbl.heading('cover', text='表紙')
+        tbl.heading('title', text='タイトル')
+        tbl.heading('number', text='ISBN-13')
+        tbl.heading('button', text='削除ボタン')
+
+        tbl.column('cover', width=150)
+        tbl.column('title', width=100)
+        tbl.column('number', width=100)
+        tbl.column('button', width=100)
+
+        tbl.insert('', 'end', values=('本A', '本Aタイトル', '本Aコード', '削除'))
+
+        tbl.bind('<ButtonRelease-1>', lambda event: self.delete_item(tbl))  # tblを引数にしてdelete_itemメソッドを呼び出す
+
+    def delete_item(self, tbl):
+        selected_item = tbl.selection()
+        if selected_item:
+            tbl.delete(selected_item)
 
 # Tkクラス生成
 root = tk.Tk()
@@ -16,39 +45,11 @@ root.title("書籍管理アプリ")
 # 画面サイズ
 root.geometry("800x500")
 
-#タイトルの作成
-label1 = tk.Label(root, text="書籍管理アプリ", font=("Helvetica", 15, "bold"))
-label1.place(x=20, y=25)
+# BookShelfScreenクラスのインスタンスを作成
+bookshelf_screen = BookShelfScreen(root)
 
-label2 = tk.Label(root, text="本棚画面", font=("Helvetica", 20, "bold"))
-label2.place(x=250, y=20)
-
-# 表をインスタンス化
-tbl = ttk.Treeview(root)
-#表の配置
-tbl.place(x=50, y=80)
-
-# 列を作成
-tbl['columns'] = ('cover', 'title', 'number', 'button')
-
-# ヘッダーの設定
-tbl['show'] = 'headings'
-tbl.heading('cover', text='表紙')
-tbl.heading('title', text='タイトル')
-tbl.heading('number', text='ISBN-13')
-tbl.heading('button', text='削除ボタン')
-
-# 各列の幅を設定
-tbl.column('cover', width=150)
-tbl.column('title', width=100)
-tbl.column('number', width=100)
-tbl.column('button', width=100)
-
-# データ設定
-tbl.insert('', 'end', values=('本A', '本Aタイトル', '本Aコード', '削除'))
-
-# ボタンクリック時の動作を設定
-tbl.bind('<ButtonRelease-1>', lambda event: delete_item())
+# BookShelfScreenを表示
+bookshelf_screen.screen_show()
 
 # メインループ実行
 root.mainloop()
