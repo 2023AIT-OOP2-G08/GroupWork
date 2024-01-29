4f2c6914a61089669121c3d33581346d53b583f0
shinno22085 <shin-nosuke0919@au.com>
2024-01-29 21:48:06 +0900

    Update BookshelfScreen.py

--- BookManagement/BookshelfScreen.py
+++ BookManagement/BookshelfScreen.py
@@ -36,18 +36,7 @@ BORDER = tk.GROOVE # 枠線
 class BookshelfScreen(BaseScreen):
 
     photos = [] # 本画像保存用リスト
-    input_data = []  # 更新されるリストをクラスに用意
-
-    """
-
-    TODO: case1
-    なにもいらない
-
-    TODO: case2
-    更新されるリストをクラスに用意しなければいけない
-    input_data = {} など
-
-    """
+    input_data = {}  # 更新されるリストをクラスに用意
 
     def create_widgets(self):
         """
@@ -56,204 +45,6 @@ class BookshelfScreen(BaseScreen):
         (BaseScreenのオーバーライド)
 
         """
-        item = BookshelfFunction.get_bookshelf(SHELF_JSON_PATH)
-        """
-
-        TODO: テストデータなので、最後は消してください。
-
-        """
-        # テストデータ
-        test_data = [
-            {
-                "title": "情熱プログラマー ソフトウェア開発者の幸せな生き方",
-                "isbn_13": "9784274067938",
-                "authors": [
-                    "Chad Fowler"
-                ],
-                "published_date": "2010-02",
-                "page_count": 200,
-                "description": "本書は、等身大のプログラマの一人がキャリア開発の重要性を説き、そのための心構えなどを示したもの。「プログラマはビジネス視点を持って意識的なキャリア開発をすべき」という視点から、その実践方法を著者独特の生き生きとした共感できる語り口で伝える。原書は\"The Passionate Programmer: Creating a Remarkable Career in Software Development\"(The Pragmatic Programmers,2009)",
-                "publisher": "株式会社 オーム社",
-                "cover_image_url": "http://books.google.com/books/content?id=oFrK2r8rQbEC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-            },
-             {
-                "title": "ハッカーと画家",
-                "isbn_13": "9784274065972",
-                "authors": "No authors available",
-                "published_date": "2005",
-                "page_count": 284,
-                "description": "天才プログラマが語る創造のセンス",
-                "publisher": "株式会社 オーム社",
-                "cover_image_url": "http://books.google.com/books/content?id=SinFRfuTH7IC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-            },
-            {
-                "title": "読みやすいコードのガイドライン -持続可能なソフトウェア開発のために",
-                "isbn_13": "9784297130367",
-                "authors": [
-                    "石川宗寿"
-                ],
-                "published_date": "2022-10-22",
-                "page_count": 0,
-                "description": "開発が大規模化・長期化するほど、コードを「読む」コストは増大していきます。そのため「読みやすさ」の向上は、生産性を改善し、プロダクトの成長限界を引き上げる重要な手段と言えるでしょう。 本書は、読みやすさの本質を学び、実践するための考え方をマスターできる一冊です。体系的な理解を実現するため、あらゆる角度から、豊富な例を交えて解説しています。表面的なテクニックではなく、いま目の前にあるコードに最適な改良方法を選び取る力が身に付きます。",
-                "publisher": "No publisher information available",
-                "cover_image_url": "http://books.google.com/books/content?id=uyNgzwEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
-            },
-            {
-                "title": "リーダブルコード",
-                "isbn_13": "9784873115658",
-                "authors": [
-                    "Dustin Boswell",
-                    "Trevor Foucher"
-                ],
-                "published_date": "2012-06",
-                "page_count": 237,
-                "description": "読んでわかるコードの重要性と方法について解説",
-                "publisher": "O'Reilly Media, Inc.",
-                "cover_image_url": "http://books.google.com/books/content?id=Wx1dLwEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
-            },
-            {
-                "title": "エンジニアのためのドキュメントライティング",
-                "isbn_13": "9784800590831",
-                "authors": [
-                    "ジャレッド・バーディ",
-                    "ザッカリー・サラ・コーライセン",
-                    "ジェン・ランボーン",
-                    "デービッド・ヌーニェス",
-                    "ハイディ・ウォーターハウス"
-                ],
-                "published_date": "2023-03-11",
-                "page_count": 0,
-                "description": "経験に長けた執筆者たちが開発者向けドキュメントの作成方法をゼロから説明するフィールドガイド",
-                "publisher": "No publisher information available",
-                "cover_image_url": "http://books.google.com/books/content?id=Mi-rzwEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
-            },
-            {
-                "title": "LaTeX2e 美文書作成入門",
-                "isbn_13": "9784297117122",
-                "authors": [
-                    "奥村晴彦",
-                    "黒木裕介"
-                ],
-                "published_date": "2020-11",
-                "page_count": 448,
-                "description": "No description available",
-                "publisher": "No publisher information available",
-                "cover_image_url": "http://books.google.com/books/content?id=zDsJzgEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
-            },
-            {
-                "title": "UIデザインの教科書［新版］ マルチデバイス時代のインターフェース設計",
-                "isbn_13": "9784798155456",
-                "authors": [
-                    "原田秀司"
-                ],
-                "published_date": "2019-01-21",
-                "page_count": 210,
-                "description": "使いやすい理由とは何か 本書はUIにおけるデザインの定義から、 ハードおよびソフトによる制約、人間の心理による影響、 そして具体的にデザインを形にする方法までを、 図や画像を使いながら、わかりやすく体系的に解説していきます。 Webサイトの閲覧者やアプリのユーザーは、 いつのまにか迷ったり、わからなくなったり、 操作がしっくりこなかったりすることがあります。 本書を読むと「わかりやすさ」と「使いやすさ」の要点がわかるので、 ユーザーを迷わせない、最適なUIを見つけることができます。 デザイナーはもちろん、エンジニア、ディレクター、発注者など、 UI制作に関わる、あらゆる方におすすめの1冊です。 ＊本書は2013年刊行の『UIデザインの教科書』をもとにしていますが、 最新環境にあわせて、構成及び内容を全面的に書き直しています。 〈こんな人のための本です〉 ・UIデザインの基本的な考え方を学びたい ・わかりやすさや使いやすさの理由が知りたい ・最新のデバイスごとの違いやルールを知りたい ・UIデザインのチェック項目が知りたい ・UIデザインを説明するためのロジックが学びたい ...etc 〈目次〉 第1章 デザインの目的とUI/UX 第2章 物理的な制約 第3章 ソフトウェアの影響 第4章 人間の認知特性 第5章 階層と構造 第6章 ナビゲーションとインタラクション 第7章 デザインを形にする",
-                "publisher": "翔泳社",
-                "cover_image_url": "http://books.google.com/books/content?id=oH2GDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-            },
-            {
-                "title": "UIデザインの教科書［新版］ マルチデバイス時代のインターフェース設計",
-                "isbn_13": "9784798155456",
-                "authors": [
-                    "原田秀司"
-                ],
-                "published_date": "2019-01-21",
-                "page_count": 210,
-                "description": "使いやすい理由とは何か 本書はUIにおけるデザインの定義から、 ハードおよびソフトによる制約、人間の心理による影響、 そして具体的にデザインを形にする方法までを、 図や画像を使いながら、わかりやすく体系的に解説していきます。 Webサイトの閲覧者やアプリのユーザーは、 いつのまにか迷ったり、わからなくなったり、 操作がしっくりこなかったりすることがあります。 本書を読むと「わかりやすさ」と「使いやすさ」の要点がわかるので、 ユーザーを迷わせない、最適なUIを見つけることができます。 デザイナーはもちろん、エンジニア、ディレクター、発注者など、 UI制作に関わる、あらゆる方におすすめの1冊です。 ＊本書は2013年刊行の『UIデザインの教科書』をもとにしていますが、 最新環境にあわせて、構成及び内容を全面的に書き直しています。 〈こんな人のための本です〉 ・UIデザインの基本的な考え方を学びたい ・わかりやすさや使いやすさの理由が知りたい ・最新のデバイスごとの違いやルールを知りたい ・UIデザインのチェック項目が知りたい ・UIデザインを説明するためのロジックが学びたい ...etc 〈目次〉 第1章 デザインの目的とUI/UX 第2章 物理的な制約 第3章 ソフトウェアの影響 第4章 人間の認知特性 第5章 階層と構造 第6章 ナビゲーションとインタラクション 第7章 デザインを形にする",
-                "publisher": "翔泳社",
-                "cover_image_url": "http://books.google.com/books/content?id=oH2GDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-            },
-            {
-                "title": "UIデザインの教科書［新版］ マルチデバイス時代のインターフェース設計",
-                "isbn_13": "9784798155456",
-
-                "authors": [
-                    "原田秀司"
-                ],
-                "published_date": "2019-01-21",
-                "page_count": 210,
-                "description": "使いやすい理由とは何か 本書はUIにおけるデザインの定義から、 ハードおよびソフトによる制約、人間の心理による影響、 そして具体的にデザインを形にする方法までを、 図や画像を使いながら、わかりやすく体系的に解説していきます。 Webサイトの閲覧者やアプリのユーザーは、 いつのまにか迷ったり、わからなくなったり、 操作がしっくりこなかったりすることがあります。 本書を読むと「わかりやすさ」と「使いやすさ」の要点がわかるので、 ユーザーを迷わせない、最適なUIを見つけることができます。 デザイナーはもちろん、エンジニア、ディレクター、発注者など、 UI制作に関わる、あらゆる方におすすめの1冊です。 ＊本書は2013年刊行の『UIデザインの教科書』をもとにしていますが、 最新環境にあわせて、構成及び内容を全面的に書き直しています。 〈こんな人のための本です〉 ・UIデザインの基本的な考え方を学びたい ・わかりやすさや使いやすさの理由が知りたい ・最新のデバイスごとの違いやルールを知りたい ・UIデザインのチェック項目が知りたい ・UIデザインを説明するためのロジックが学びたい ...etc 〈目次〉 第1章 デザインの目的とUI/UX 第2章 物理的な制約 第3章 ソフトウェアの影響 第4章 人間の認知特性 第5章 階層と構造 第6章 ナビゲーションとインタラクション 第7章 デザインを形にする",
-                "publisher": "翔泳社",
-                "cover_image_url": "http://books.google.com/books/content?id=oH2GDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-            },
-            {
-                "title": "UIデザインの教科書［新版］ マルチデバイス時代のインターフェース設計",
-                "isbn_13": "9784798155456",
-                "authors": [
-                    "原田秀司"
-                ],
-                "published_date": "2019-01-21",
-                "page_count": 210,
-                "description": "使いやすい理由とは何か 本書はUIにおけるデザインの定義から、 ハードおよびソフトによる制約、人間の心理による影響、 そして具体的にデザインを形にする方法までを、 図や画像を使いながら、わかりやすく体系的に解説していきます。 Webサイトの閲覧者やアプリのユーザーは、 いつのまにか迷ったり、わからなくなったり、 操作がしっくりこなかったりすることがあります。 本書を読むと「わかりやすさ」と「使いやすさ」の要点がわかるので、 ユーザーを迷わせない、最適なUIを見つけることができます。 デザイナーはもちろん、エンジニア、ディレクター、発注者など、 UI制作に関わる、あらゆる方におすすめの1冊です。 ＊本書は2013年刊行の『UIデザインの教科書』をもとにしていますが、 最新環境にあわせて、構成及び内容を全面的に書き直しています。 〈こんな人のための本です〉 ・UIデザインの基本的な考え方を学びたい ・わかりやすさや使いやすさの理由が知りたい ・最新のデバイスごとの違いやルールを知りたい ・UIデザインのチェック項目が知りたい ・UIデザインを説明するためのロジックが学びたい ...etc 〈目次〉 第1章 デザインの目的とUI/UX 第2章 物理的な制約 第3章 ソフトウェアの影響 第4章 人間の認知特性 第5章 階層と構造 第6章 ナビゲーションとインタラクション 第7章 デザインを形にする",
-                "publisher": "翔泳社",
-                "cover_image_url": "http://books.google.com/books/content?id=oH2GDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-            },
-            {
-                "title": "UIデザインの教科書［新版］ マルチデバイス時代のインターフェース設計",
-                "isbn_13": "9784798155456",
-                "authors": [
-                    "原田秀司"
-                ],
-                "published_date": "2019-01-21",
-                "page_count": 210,
-                "description": "使いやすい理由とは何か 本書はUIにおけるデザインの定義から、 ハードおよびソフトによる制約、人間の心理による影響、 そして具体的にデザインを形にする方法までを、 図や画像を使いながら、わかりやすく体系的に解説していきます。 Webサイトの閲覧者やアプリのユーザーは、 いつのまにか迷ったり、わからなくなったり、 操作がしっくりこなかったりすることがあります。 本書を読むと「わかりやすさ」と「使いやすさ」の要点がわかるので、 ユーザーを迷わせない、最適なUIを見つけることができます。 デザイナーはもちろん、エンジニア、ディレクター、発注者など、 UI制作に関わる、あらゆる方におすすめの1冊です。 ＊本書は2013年刊行の『UIデザインの教科書』をもとにしていますが、 最新環境にあわせて、構成及び内容を全面的に書き直しています。 〈こんな人のための本です〉 ・UIデザインの基本的な考え方を学びたい ・わかりやすさや使いやすさの理由が知りたい ・最新のデバイスごとの違いやルールを知りたい ・UIデザインのチェック項目が知りたい ・UIデザインを説明するためのロジックが学びたい ...etc 〈目次〉 第1章 デザインの目的とUI/UX 第2章 物理的な制約 第3章 ソフトウェアの影響 第4章 人間の認知特性 第5章 階層と構造 第6章 ナビゲーションとインタラクション 第7章 デザインを形にする",
-                "publisher": "翔泳社",
-                "cover_image_url": "http://books.google.com/books/content?id=oH2GDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-            },
-            {
-                "title": "UIデザインの教科書［新版］ マルチデバイス時代のインターフェース設計",
-                "isbn_13": "9784798155456",
-                "authors": [
-                    "原田秀司"
-                ],
-                "published_date": "2019-01-21",
-                "page_count": 210,
-                "description": "使いやすい理由とは何か 本書はUIにおけるデザインの定義から、 ハードおよびソフトによる制約、人間の心理による影響、 そして具体的にデザインを形にする方法までを、 図や画像を使いながら、わかりやすく体系的に解説していきます。 Webサイトの閲覧者やアプリのユーザーは、 いつのまにか迷ったり、わからなくなったり、 操作がしっくりこなかったりすることがあります。 本書を読むと「わかりやすさ」と「使いやすさ」の要点がわかるので、 ユーザーを迷わせない、最適なUIを見つけることができます。 デザイナーはもちろん、エンジニア、ディレクター、発注者など、 UI制作に関わる、あらゆる方におすすめの1冊です。 ＊本書は2013年刊行の『UIデザインの教科書』をもとにしていますが、 最新環境にあわせて、構成及び内容を全面的に書き直しています。 〈こんな人のための本です〉 ・UIデザインの基本的な考え方を学びたい ・わかりやすさや使いやすさの理由が知りたい ・最新のデバイスごとの違いやルールを知りたい ・UIデザインのチェック項目が知りたい ・UIデザインを説明するためのロジックが学びたい ...etc 〈目次〉 第1章 デザインの目的とUI/UX 第2章 物理的な制約 第3章 ソフトウェアの影響 第4章 人間の認知特性 第5章 階層と構造 第6章 ナビゲーションとインタラクション 第7章 デザインを形にする",
-                "publisher": "翔泳社",
-                "cover_image_url": "http://books.google.com/books/content?id=oH2GDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-            },
-            {
-                "title": "UIデザインの教科書［新版］ マルチデバイス時代のインターフェース設計",
-                "isbn_13": "9784798155456",
-                "authors": [
-                    "原田秀司"
-                ],
-                "published_date": "2019-01-21",
-                "page_count": 210,
-                "description": "使いやすい理由とは何か 本書はUIにおけるデザインの定義から、 ハードおよびソフトによる制約、人間の心理による影響、 そして具体的にデザインを形にする方法までを、 図や画像を使いながら、わかりやすく体系的に解説していきます。 Webサイトの閲覧者やアプリのユーザーは、 いつのまにか迷ったり、わからなくなったり、 操作がしっくりこなかったりすることがあります。 本書を読むと「わかりやすさ」と「使いやすさ」の要点がわかるので、 ユーザーを迷わせない、最適なUIを見つけることができます。 デザイナーはもちろん、エンジニア、ディレクター、発注者など、 UI制作に関わる、あらゆる方におすすめの1冊です。 ＊本書は2013年刊行の『UIデザインの教科書』をもとにしていますが、 最新環境にあわせて、構成及び内容を全面的に書き直しています。 〈こんな人のための本です〉 ・UIデザインの基本的な考え方を学びたい ・わかりやすさや使いやすさの理由が知りたい ・最新のデバイスごとの違いやルールを知りたい ・UIデザインのチェック項目が知りたい ・UIデザインを説明するためのロジックが学びたい ...etc 〈目次〉 第1章 デザインの目的とUI/UX 第2章 物理的な制約 第3章 ソフトウェアの影響 第4章 人間の認知特性 第5章 階層と構造 第6章 ナビゲーションとインタラクション 第7章 デザインを形にする",
-                "publisher": "翔泳社",
-                "cover_image_url": "http://books.google.com/books/content?id=oH2GDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-            },
-            {
-                "title": "UIデザインの教科書［新版］ マルチデバイス時代のインターフェース設計",
-                "isbn_13": "9784798155456",
-                "authors": [
-                    "原田秀司"
-                ],
-                "published_date": "2019-01-21",
-                "page_count": 210,
-                "description": "使いやすい理由とは何か 本書はUIにおけるデザインの定義から、 ハードおよびソフトによる制約、人間の心理による影響、 そして具体的にデザインを形にする方法までを、 図や画像を使いながら、わかりやすく体系的に解説していきます。 Webサイトの閲覧者やアプリのユーザーは、 いつのまにか迷ったり、わからなくなったり、 操作がしっくりこなかったりすることがあります。 本書を読むと「わかりやすさ」と「使いやすさ」の要点がわかるので、 ユーザーを迷わせない、最適なUIを見つけることができます。 デザイナーはもちろん、エンジニア、ディレクター、発注者など、 UI制作に関わる、あらゆる方におすすめの1冊です。 ＊本書は2013年刊行の『UIデザインの教科書』をもとにしていますが、 最新環境にあわせて、構成及び内容を全面的に書き直しています。 〈こんな人のための本です〉 ・UIデザインの基本的な考え方を学びたい ・わかりやすさや使いやすさの理由が知りたい ・最新のデバイスごとの違いやルールを知りたい ・UIデザインのチェック項目が知りたい ・UIデザインを説明するためのロジックが学びたい ...etc 〈目次〉 第1章 デザインの目的とUI/UX 第2章 物理的な制約 第3章 ソフトウェアの影響 第4章 人間の認知特性 第5章 階層と構造 第6章 ナビゲーションとインタラクション 第7章 デザインを形にする",
-                "publisher": "翔泳社",
-                "cover_image_url": "http://books.google.com/books/content?id=oH2GDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-            },
-            {
-                "title": "UIデザインの教科書［新版］ マルチデバイス時代のインターフェース設計マルチデバイス時代のインターフェース設計マルチデバイス時代のインターフェース設計マルチデバイス時代のインターフェース設計マルチデバイス時代のインターフェース設計　",
-                "isbn_13": "9784798155456",
-                "authors": [
-                    "原田秀司"
-                ],
-                "published_date": "2019-01-21",
-                "page_count": 210,
-                "description": "使いやすい理由とは何か 本書はUIにおけるデザインの定義から、 ハードおよびソフトによる制約、人間の心理による影響、 そして具体的にデザインを形にする方法までを、 図や画像を使いながら、わかりやすく体系的に解説していきます。 Webサイトの閲覧者やアプリのユーザーは、 いつのまにか迷ったり、わからなくなったり、 操作がしっくりこなかったりすることがあります。 本書を読むと「わかりやすさ」と「使いやすさ」の要点がわかるので、 ユーザーを迷わせない、最適なUIを見つけることができます。 デザイナーはもちろん、エンジニア、ディレクター、発注者など、 UI制作に関わる、あらゆる方におすすめの1冊です。 ＊本書は2013年刊行の『UIデザインの教科書』をもとにしていますが、 最新環境にあわせて、構成及び内容を全面的に書き直しています。 〈こんな人のための本です〉 ・UIデザインの基本的な考え方を学びたい ・わかりやすさや使いやすさの理由が知りたい ・最新のデバイスごとの違いやルールを知りたい ・UIデザインのチェック項目が知りたい ・UIデザインを説明するためのロジックが学びたい ...etc 〈目次〉 第1章 デザインの目的とUI/UX 第2章 物理的な制約 第3章 ソフトウェアの影響 第4章 人間の認知特性 第5章 階層と構造 第6章 ナビゲーションとインタラクション 第7章 デザインを形にする",
-                "publisher": "翔泳社",
-                "cover_image_url": "http://books.google.com/books/content?id=oH2GDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-            }
-        ]
-
-        # TODO: case2 - input_dataにデータを設定
-        self.input_data = test_data  # または、他の方法でデータを取得
-
         ### 画面設定
         self.root.title('本棚') # 画面タイトル
         self.root.geometry(SCREEN_SIZE) # 画面サイズ
@@ -310,8 +101,8 @@ class BookshelfScreen(BaseScreen):
 
         ### 表のフレーム
         # debug用bg付き
-        bookself_table_canvas = tk.Canvas(book_self_screen_frame, bg='green') # スクロールバーの為のキャンバス
-        bookself_table_frame = tk.Frame(bookself_table_canvas,bg='red', width=840, height=0) # 検索結果フレーム(初期設定)
+        bookself_table_canvas = tk.Canvas(book_self_screen_frame) # スクロールバーの為のキャンバス
+        bookself_table_frame = tk.Frame(bookself_table_canvas, width=840, height=0) # 検索結果フレーム(初期設定)
         scrollbar = tk.Scrollbar(bookself_table_canvas, orient=tk.VERTICAL, command=bookself_table_canvas.yview) # スクロールバー
 
         """
@@ -385,33 +176,14 @@ class BookshelfScreen(BaseScreen):
         def on_delete_button_click(item, index): # 本棚から削除
             """
 
-            TODO: case1
-            itemを受け取って、json内のデータを消す。
-            検索画面と似たような実装ならば、ここではjson内のデータを消し、表を更新するだけ
-            (重そう、update_table内でmoduleからリストを毎回受け取る。)
-
-            TODO: case2
-            item&何かを受け取って、listの中身と、json内のデータを消す。
-            indexの取得がわからない...
-            (こっちの方が軽そう、update_table内で、リストを更新するだけ)
-
-            消す作業自体は同じくらいだが、case2だとjsonを読み込むという工程が一度で済む。
-            (一つだけ消す時にmoduleで全部読み込んでたらおなじかも)
+            本棚から削除
+            
+            listの中身と、json内のデータを消す。
 
             """
-            # # 指定されたインデックスの本を削除
-            # Processor.remove_index_elements(item, index - 3)  # -3 は enumerate で start=3 を指定しているため
-
-            #削除前のデータを取得
-            search_book_data = Processor.get_bookshelf(SHELF_JSON_PATH)
-            # 指定されたインデックスの本を削除
 
             # BookshelfFunction.remove_index_elements を呼び出し、削除後のデータを取得
-            search_book_data = Processor.remove_index_elements(SHELF_JSON_PATH, search_book_data, index)
-
-
-             # Save the updated shelf data back to the JSON file
-            Processor.save_bookshelf(SHELF_JSON_PATH, search_book_data)
+            BookshelfScreen.input_data = Processor.remove_index_elements(SHELF_JSON_PATH, BookshelfScreen.input_data, index - 1)
 
             update_table()  # 画面の更新を行う関数を呼び出す
 
@@ -423,28 +195,9 @@ class BookshelfScreen(BaseScreen):
             初期化して、再設定
 
             """
-
-            """
-
-            TODO: case1
-            ここで、リストを受け取る
-            moduleを利用し、リストを受け取る。
-            bookself_data = bookself_data = Processor.get_bookshelf
-            
-            TODO: case2
-            classに用意されているリストの中身は既に変わっているので、
-            classのinput_dataをこの関数のbookself_dataにいれればいい。
-            bookself_data = BookselfTest.input_data
-            だけでいい
-
-            ↓↓↓↓↓これはtest用
-            """
-            bookself_data = test_data # test用
-
-            # bookself_data = bookself_data = Processor.get_bookshelf
-
-            # bookself_data = self.input_data  # TODO: case2
-
+            # dataの受け取り
+            bookself_data = BookshelfScreen.input_data 
+            # print(bookself_data) # debug
 
             ### 初期化
             for widget in bookself_table_frame.winfo_children(): # table_frame内のすべての子ウィジェットを破棄
@@ -487,7 +240,7 @@ class BookshelfScreen(BaseScreen):
 
                 # 削除ボタン
                 button_border_label = tk.Label(bookself_table_frame, relief=BORDER, width=10) # 枠線
-                register_button = tk.Button(bookself_table_frame, text="削除", command=lambda item=item, index=i: on_delete_button_click(item, index))
+                register_button = tk.Button(bookself_table_frame, text="削除", command=lambda item=item, i=i: on_delete_button_click(item, i))
                 button_border_label.grid(row=i, column=3, sticky=tk.NSEW) # 枠線
                 register_button.grid(row=i, column=3)
             
@@ -502,30 +255,19 @@ class BookshelfScreen(BaseScreen):
 
         諸々の配置
 
-        update_buttonはtest用です。
-        最後は消してください。
-
         """
-
-        update_button = tk.Button(book_self_screen_frame, text="表の更新", command=update_table) # debug
-        update_button.pack() # debug
 
         scrollbar.pack(side=tk.RIGHT, fill=tk.Y) # スクロールバー
         bookself_table_canvas.pack(expand=True, fill=tk.BOTH) # キャンバス
         bookself_table_canvas.create_window((0, 0), window=bookself_table_frame, anchor="nw") # キャンバスにフレームを設置
 
-        """
-
-        TODO: case1
-        なにもいらない
-
-        TODO: case2
-        jsonをここで読み込む
-        (input_dataに)
-        
-        """
-        self.input_data = BookshelfFunction.get_bookshelf(SHELF_JSON_PATH)
+        # 最初に一度jsonを読み込む
+        BookshelfScreen.input_data = BookshelfFunction.get_bookshelf(SHELF_JSON_PATH)
         update_table() # 最初に呼んでおく。
+
+
+
+
 
 """
 
@@ -544,4 +286,3 @@ if __name__ == '__main__':
     book_self_screen.screen_show() # 画面を設定
 
     root.mainloop() # イベントループ
-
